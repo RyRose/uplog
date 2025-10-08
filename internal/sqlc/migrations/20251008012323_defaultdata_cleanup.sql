43a69608ac2d471393ca5856ff41b9a8a2d228b8
RyRose <RyRose@users.noreply.github.com>
2025-10-07 20:26:03 -0500

    feat: initial data cleanup

---
+++ internal/sqlc/migrations/20251008012323_defaultdata_cleanup.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+-- +goose StatementBegin
+INSERT OR REPLACE INTO template_variable VALUES('PUSH','TODO');
+INSERT OR REPLACE INTO template_variable VALUES('PULL','TODO');
+INSERT OR REPLACE INTO template_variable VALUES('CORE','TODO');
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+
+-- Irreversible... good luck!
+
+-- +goose StatementEnd
+
