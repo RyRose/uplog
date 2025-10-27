882441e9d666cb7b7839649012cd437c57af21c0
RyRose <RyRose@users.noreply.github.com>
2025-10-26 22:52:08 -0500

    fix: add version to swagger url for cache busting, also use autoversion

--- config/main.lua
+++ config/main.lua
@@ -2,14 +2,16 @@ local env = require("config.env")
 local types = require("config.types")
 
 local port = env.Or("PORT", "8080")
+local curtime = os.time()
+local version = env.Or("VERSION", "auto-" .. tostring(curtime))
 
 ---@type Data
 local M = {
 	debug = env.Or("DEBUG", false),
-	version = env.Get("VERSION", types.String),
+	version = version,
 	database_path = env.Or("DATABASE_PATH", "./tmp/db/data.db"),
 	port = port,
-	swagger_url = env.Or("SWAGGER_URL", "http://localhost:" .. port .. "/docs/swagger.json"),
+	swagger_url = env.Or("SWAGGER_URL", "http://localhost:" .. port .. "/docs/swagger.json") .. "?v=" .. version,
 }
 
 return M
